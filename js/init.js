5569b9a4fc771ba2116422a2419793c8059b7307
clencyc <clency2023@gmail.com>
2024-07-11 19:39:49 +0300

    added socials

--- js/init.js
+++ js/init.js
@@ -1,7 +1,6 @@
 //Hook up the tweet display
 
-$(document).ready(function() {
-						   
+$(document).ready(function() {			   
 	$(".countdown").countdown({
 				date: "8 July 2048 18:30:00",
 				format: "on"
